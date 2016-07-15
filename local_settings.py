b81b591dbfecab84c44088779662c142bc0fffa1
iampixeldiva <pixeldiva@gmail.com>
2016-07-15 14:49:44 +0100

    adding ebooks files

---
+++ local_settings.py
@@ -0,0 +1,10 @@
+#configuration
+MY_CONSUMER_KEY = '	iYQePetIZkbggYuPqZoSibk0y'
+MY_CONSUMER_SECRET = 'jszqOFmtzJlxqjKXmJhiq82DCa6qBnBQV7ssuEf2hwa4GldShi'
+MY_ACCESS_TOKEN_KEY = '753938783368781824-0CCifB94Vrg5JlhKJs8XFCMW62wFK6y'
+MY_ACCESS_TOKEN_SECRET = 'Y842vpubCnyfLLAswhyom6YgYlB93n1QJKS0ybR5GuzWx'
+
+SOURCE_ACCOUNT = "@pixeldiva" #The Twitter handle of the account that you'll generate tweets based on.
+ODDS = 8 #How often do you want this to run? 1/8 times?
+ORDER = 2 #how closely do you want this to hew to sensical? 1 is low and 3 is high.
+DEBUG = True #Set this to False to start Tweeting live
