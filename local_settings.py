4f7a1dbd3ddff3da7b989a6edf25d4e129b511ae
iampixeldiva <pixeldiva@gmail.com>
2016-07-15 14:55:35 +0100

    start tweeting

--- local_settings.py
+++ local_settings.py
@@ -7,4 +7,4 @@ MY_ACCESS_TOKEN_SECRET = 'Y842vpubCnyfLLAswhyom6YgYlB93n1QJKS0ybR5GuzWx'
 SOURCE_ACCOUNT = "@pixeldiva" #The Twitter handle of the account that you'll generate tweets based on.
 ODDS = 8 #How often do you want this to run? 1/8 times?
 ORDER = 2 #how closely do you want this to hew to sensical? 1 is low and 3 is high.
-DEBUG = True #Set this to False to start Tweeting live
+DEBUG = False #Set this to False to start Tweeting live
