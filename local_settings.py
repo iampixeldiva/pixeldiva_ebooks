d5b4201be425411ea0f9ce5a385ecb5602009b9b
iampixeldiva <pixeldiva@gmail.com>
2016-07-15 15:20:17 +0100

    change to debut and run always

--- local_settings.py
+++ local_settings.py
@@ -5,6 +5,6 @@ MY_ACCESS_TOKEN_KEY = '753938783368781824-0CCifB94Vrg5JlhKJs8XFCMW62wFK6y'
 MY_ACCESS_TOKEN_SECRET = 'Y842vpubCnyfLLAswhyom6YgYlB93n1QJKS0ybR5GuzWx'
 
 SOURCE_ACCOUNT = "@pixeldiva" #The Twitter handle of the account that you'll generate tweets based on.
-ODDS = 8 #How often do you want this to run? 1/8 times?
+ODDS = 0 #How often do you want this to run? 1/8 times?
 ORDER = 2 #how closely do you want this to hew to sensical? 1 is low and 3 is high.
-DEBUG = False #Set this to False to start Tweeting live
+DEBUG = True #Set this to False to start Tweeting live
