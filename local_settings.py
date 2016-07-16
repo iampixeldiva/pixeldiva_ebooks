ee884a46854744704819c90c5daf2df7dbe3c2ec
iampixeldiva <pixeldiva@gmail.com>
2016-07-16 09:53:01 +0100

    New keys. Let's do this.

--- local_settings.py
+++ local_settings.py
@@ -1,10 +1,10 @@
 #configuration
-MY_CONSUMER_KEY = '9DoTlvn8SYFgkhCMwI8TtZ9Y7'
-MY_CONSUMER_SECRET = 'EvHMJGghuedyzQuIbQtxdj9rqxl6bN3mn3FXXI9AY3MQM7tZYa'
-MY_ACCESS_TOKEN_KEY = '753938783368781824-0CCifB94Vrg5JlhKJs8XFCMW62wFK6y'
-MY_ACCESS_TOKEN_SECRET = 'Y842vpubCnyfLLAswhyom6YgYlB93n1QJKS0ybR5GuzWx'
+MY_CONSUMER_KEY = 'WtkXT9CskGjYPdfl3R8oYAvqb'
+MY_CONSUMER_SECRET = 'GMocL4fj0y8OofvQArYQzL6LDgKr9Fksyq7fWXZc9tjbk3yy68'
+MY_ACCESS_TOKEN_KEY = '753938783368781824-RCXkNnt7uZb92SX4meXZO0jRKI605Te'
+MY_ACCESS_TOKEN_SECRET = 'pGslUk5VgtXwNGTpT9gOhnGjJLg0tt2pXwelXlpoEmvXV'
 
 SOURCE_ACCOUNT = "pixeldiva" #The Twitter handle of the account that you'll generate tweets based on.
 ODDS = 0 #How often do you want this to run? 1/8 times?
 ORDER = 2 #how closely do you want this to hew to sensical? 1 is low and 3 is high.
-DEBUG = True #Set this to False to start Tweeting live
+DEBUG = False #Set this to False to start Tweeting live
