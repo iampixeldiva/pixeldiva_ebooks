f8e293e33157f71afd1a822eb612dd229fb3ee98
iampixeldiva <pixeldiva@gmail.com>
2016-07-16 09:56:04 +0100

    new access tokens
    still got invalid token error after changing consumer keys.

--- local_settings.py
+++ local_settings.py
@@ -1,8 +1,8 @@
 #configuration
 MY_CONSUMER_KEY = 'a4bER2U7NEGYMlpPcbkiyZZMO'
 MY_CONSUMER_SECRET = 'smFfH3wiKkLYx5tR58sbHm9Wpw2wfU5Iyl0oMEuZnzL4kp4kyW'
-MY_ACCESS_TOKEN_KEY = '753938783368781824-RCXkNnt7uZb92SX4meXZO0jRKI605Te'
-MY_ACCESS_TOKEN_SECRET = 'pGslUk5VgtXwNGTpT9gOhnGjJLg0tt2pXwelXlpoEmvXV'
+MY_ACCESS_TOKEN_KEY = '753938783368781824-zkfDzp6LGxs1hXiRRkcauDLC1by3zDP'
+MY_ACCESS_TOKEN_SECRET = '35mYehYuHgW5vU8nv8gR4ju7hQlZps7WfsUMarUT2tCy5'
 
 SOURCE_ACCOUNT = "pixeldiva" #The Twitter handle of the account that you'll generate tweets based on.
 ODDS = 0 #How often do you want this to run? 1/8 times?
