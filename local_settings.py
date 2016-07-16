e259beef89531941833bb5b7f9d12fc08aa0a601
iampixeldiva <pixeldiva@gmail.com>
2016-07-16 10:03:32 +0100

    @

--- local_settings.py
+++ local_settings.py
@@ -4,7 +4,7 @@ MY_CONSUMER_SECRET = 'W5vFllr4a5PdWi13kSw2SX0WadohXjM4XXSd8ry9ZloiIuqHEh'
 MY_ACCESS_TOKEN_KEY = '753938783368781824-Nd5WL7X22P59uckcaTZ8LbWDxtpduyd'
 MY_ACCESS_TOKEN_SECRET = '7yptLmn7gUZFdoHd9ARvVTuesisv7KcMptlaRjkkZ2WPM'
 
-SOURCE_ACCOUNT = "pixeldiva" #The Twitter handle of the account that you'll generate tweets based on.
+SOURCE_ACCOUNT = "@pixeldiva" #The Twitter handle of the account that you'll generate tweets based on.
 ODDS = 0 #How often do you want this to run? 1/8 times?
 ORDER = 2 #how closely do you want this to hew to sensical? 1 is low and 3 is high.
 DEBUG = False #Set this to False to start Tweeting live
