8e50df9037172c2fb9ebe57df558cc2182158e1d
iampixeldiva <pixeldiva@gmail.com>
2016-07-16 10:02:50 +0100

    new keys please

--- local_settings.py
+++ local_settings.py
@@ -1,8 +1,8 @@
 #configuration
-MY_CONSUMER_KEY = 'a4bER2U7NEGYMlpPcbkiyZZMO'
-MY_CONSUMER_SECRET = 'smFfH3wiKkLYx5tR58sbHm9Wpw2wfU5Iyl0oMEuZnzL4kp4kyW'
-MY_ACCESS_TOKEN_KEY = '753938783368781824-zkfDzp6LGxs1hXiRRkcauDLC1by3zDP'
-MY_ACCESS_TOKEN_SECRET = '35mYehYuHgW5vU8nv8gR4ju7hQlZps7WfsUMarUT2tCy5'
+MY_CONSUMER_KEY = 'IH95WZ8uzfUhtv0ZAHlXyJP25'
+MY_CONSUMER_SECRET = 'W5vFllr4a5PdWi13kSw2SX0WadohXjM4XXSd8ry9ZloiIuqHEh'
+MY_ACCESS_TOKEN_KEY = '753938783368781824-Nd5WL7X22P59uckcaTZ8LbWDxtpduyd'
+MY_ACCESS_TOKEN_SECRET = '7yptLmn7gUZFdoHd9ARvVTuesisv7KcMptlaRjkkZ2WPM'
 
 SOURCE_ACCOUNT = "pixeldiva" #The Twitter handle of the account that you'll generate tweets based on.
 ODDS = 0 #How often do you want this to run? 1/8 times?
